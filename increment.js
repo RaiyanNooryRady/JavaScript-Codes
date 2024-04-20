8e1967beb006920e4c753893d920855444bb0456
DaiyanNDahy <daiyanndahy@gmail.com>
2024-04-20 13:04:38 +0600

    added some style

--- increment.js
+++ increment.js
@@ -1,8 +1,30 @@
 const domContainer = document.querySelector("#root");
 
-const myElement = React.createElement("div",null,[
-    React.createElement("p", null, "hello tension"),
-    React.createElement("p", null, "hello no tension"),
-]);
-
-ReactDOM.render(myElement, domContainer);
+// const myElement = React.createElement("div",null,[
+//     React.createElement("p", null, "hello tension"),
+//     React.createElement("h", null, "hello no tension"),
+// ]);
+const style = {
+    color: 'black',
+    fontSize: '50px',
+    textAlign: 'center',
+    margin: '200px'
+};
+const buttonStyle = {
+    fontSize:'50px',
+    color:'white',
+    backgroundColor:'blue',
+    padding:'20px',
+    borderRadius:'10px',
+    cursor: 'pointer'
+}
+const myElement=(
+    
+    <div style={style}>
+        <h1 id="display">0</h1>
+        <div>
+            <button id="button" style={buttonStyle}>increment++</button>
+        </div>
+    </div>
+);
+ ReactDOM.render(myElement, domContainer);
