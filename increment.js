8deedc61cf7ab98bcb823ce64625e178188703ab
DaiyanNDahy <daiyanndahy@gmail.com>
2024-04-20 15:55:28 +0600

    multiple like button with different states

--- increment.js
+++ increment.js
@@ -8,7 +8,7 @@ const style = {
     color: 'black',
     fontSize: '50px',
     textAlign: 'center',
-    margin: '200px'
+    
 };
 const buttonStyle = {
     fontSize:'50px',
@@ -18,20 +18,43 @@ const buttonStyle = {
     borderRadius:'10px',
     cursor: 'pointer'
 }
-const myElement=(
-    
-    <div style={style}>
-        <h1 id="display">0</h1>
+const Increment = ()=>{
+    const [counter, setCounter]= React.useState(0);
+    //console.log(counter,setCounter);
+    console.log(React.useState(0));
+    return(
+        <div style={style}>
+        <h1 id="display">{counter}</h1>
         <div>
-            <button id="button" style={buttonStyle}>increment++</button>
+            <button id="button" onClick={()=> setCounter(counter+1)} style={buttonStyle}>Like</button>
         </div>
     </div>
+    );
+};
+// const myElement=(
+    
+//     <div style={style}>
+//         <h1 id="display">0</h1>
+//         <div>
+//             <button id="button" style={buttonStyle}>increment++</button>
+//         </div>
+//     </div>
+// );
+
+ ReactDOM.render(
+ <div className="container">
+    <Increment />
+    <Increment />
+    <Increment />
+ </div>,
+domContainer
 );
- ReactDOM.render(myElement, domContainer);
- const button= document.querySelector('#button');
- const display=document.querySelector('#display');
- let displayVal=0;
- button.addEventListener('click',()=>{
-    displayVal++;
-    display.innerHTML=displayVal;
- })
+
+//  const button= document.querySelector('#button');
+//  const display=document.querySelector('#display');
+//  let displayVal=0;
+//  button.addEventListener('click',()=>{
+//     displayVal++;
+//     display.textContent=displayVal;
+//     //display.innerHTML=displayVal; (same as last line)
+//  })
