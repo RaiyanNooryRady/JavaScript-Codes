7f35dba5055f05e08ba8dde33e2e36314f540866
DaiyanNDahy <daiyanndahy@gmail.com>
2024-04-23 12:53:54 +0600

    ternary op

--- cosntructor.js
+++ cosntructor.js
@@ -1,10 +1,15 @@
-function person (name){
-    this.name=name;
-    console.log(this.name);
+function person(name) {
+  this.name = name;
+  console.log(this.name);
 }
 //let person=(name)=> console.log(name); //error
-let sakib=new person('Sakib');
+let sakib = new person("Sakib");
 console.log(typeof sakib);
 //ternary operator
-let type= (typeof sakib=='object')?'sakib is an object':'sakib is not an object';
+let type =
+  typeof sakib == "object" ? "sakib is an object" : "sakib is not an object";
 console.log(type);
+
+let age = 10;
+let checkAge = age >= 18 ? "adult" : age < 12 ? "child" : "young";
+console.log(checkAge);
