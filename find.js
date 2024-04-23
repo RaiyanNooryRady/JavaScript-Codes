8bbc3bf262c4b21dfa1ad665485538467cee989f
DaiyanNDahy <daiyanndahy@gmail.com>
2024-04-23 14:52:05 +0600

    array find

---
+++ find.js
@@ -0,0 +1,27 @@
+let arr=[2,45,53,34,55,43];
+let x= arr.find(function(value){
+    return value>30;
+},this);
+console.log(x);
+class student{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    
+    show(){
+        console.log(`student name is ${this.name} and student age is ${this.age}`);
+    }
+
+    example(){
+        let numbers=[234,4,43,35,34];
+        // numbers.find(function(){
+        //     this.show();
+        // },this);
+        numbers.find(()=>{
+            this.show();
+        });
+    }
+}
+let raiyan= new student('Raiyan',25);
+raiyan.example();
