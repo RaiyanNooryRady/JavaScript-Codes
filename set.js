4bc25868f39905eaf132b282cfb7799203853308
DaiyanNDahy <daiyanndahy@gmail.com>
2024-04-24 14:47:12 +0600

    found unique elements of the array

--- set.js
+++ set.js
@@ -1,7 +1,7 @@
-function log(anything){
+function log(anything) {
     console.log(anything);
 }
-let mySet= new Set();//constructor syntax
+let mySet = new Set();//constructor syntax
 mySet.add(5);
 mySet.add(4);
 mySet.add(`hello`);
@@ -12,11 +12,26 @@ mySet.add(3).add(2).add(1).delete(2);
 log(mySet);
 log(mySet.size);
 //mySet.clear();
-let myArr=[24,5,4,32];
-mySet=new Set(myArr);
-let yourSet=new Set('bangladesh');
+let myArr = [24, 5, 4, 32];
+mySet = new Set(myArr); //array to set
+let yourSet = new Set('bangladesh');
 log(mySet);
 log(yourSet);
-for(let value of mySet){
+for (let value of mySet) {
     log(value);
 }
+log([...mySet]);//set to array
+let object = {
+    a: 2,
+    b: 3
+}
+log(mySet.add({
+    a: 2,
+    b: 3
+}).add({
+    a: 2,
+    b: 3
+}).add(object).add(object)); //see output.instead of 4 objects, 3 objects will be added because here object is used as reference. (for 1st and 2nd one they have different reference. for last two object they have same reference)
+let newArr=[0,0,1,2,3,3,3,3,4,4,4,5,5,5,6,6,7,7,8,9,9,9];
+let newSet= new Set(newArr);//array to set
+log([...newSet]); //set to array. found elements of the array uniquely
