0ddaed5606645bc35f95303ea2ac131f14cf8aa3
DaiyanNDahy <daiyanndahy@gmail.com>
2024-04-24 17:55:12 +0600

    copywithin

---
+++ copyWithin.js
@@ -0,0 +1,7 @@
+function log(x){
+    console.log(x);
+}
+let a=['hello','world','ok','go','there','bro','tumi','koi','jao'];
+//a.copyWithin(4,1,3); //target,start,end
+a.copyWithin(-5,2,-4);
+log(a);
