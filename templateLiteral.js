6d825e65bffd703e0e79add7bc0641a87428757c
DaiyanNDahy <daiyanndahy@gmail.com>
2024-04-24 11:48:04 +0600

    tag template literal

--- templateLiteral.js
+++ templateLiteral.js
@@ -1,6 +1,15 @@
 let x=4;
 let y=2;
-console.log(`the addition of ${x} and ${y} is ${x+y} and 
+function modifier(strings,...values){
+    // console.log(strings);
+    // console.log(values);
+    const m= strings.reduce((prevVal,currentVal)=>{
+        return prevVal+currentVal+(values.length?"value "+values.shift():"");
+    },"");
+    //console.log(m);
+    return m;
+}
+console.log(modifier `the addition of ${x} and ${y} is ${x+y} and 
 subtraction is ${x-y} and
 multiplication is ${x*y} and
-division is ${x/y}`);
+division is ${x/y} and then finish.`);
