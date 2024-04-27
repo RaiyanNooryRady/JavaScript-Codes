451a97ba751e72c51b8bd0f86a89a24420f23792
DaiyanNDahy <daiyanndahy@gmail.com>
2024-04-27 12:33:05 +0600

    functional component practise

--- React/my-react/src/index.js
+++ React/my-react/src/index.js
@@ -14,20 +14,42 @@ import ReactDOM from "react-dom/client";
 // }//react component
 //function= react component, it returns react element
 
-//class component: its stateful
-class Clock extends React.Component{
+// //class component: its stateful
+// class Clock extends React.Component{
 
-    render(){
+//     render(){
 
-        return (
-            <h1 className="heading">
-                <span className="text">Hello {this.props.children}{ new Date().toLocaleTimeString(this.props.locale)}</span> <br/>
-                <span className={this.props.className}>second is {new Date().getSeconds()}</span>
-            </h1>
-        );
-    }
-}
-// const ClockComponent= new Clock();
+//         return (
+//             <h1 className="heading">
+//                 <span className="text">Hello {this.props.children}{ new Date().toLocaleTimeString(this.props.locale)}</span> <br/>
+//                 <span className={this.props.className}>second is {new Date().getSeconds()}</span>
+//             </h1>
+//         );
+//     }
+// }
+// // const ClockComponent= new Clock();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Clock locale="bn-BD" className="hello"> World </Clock>);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Clock locale="bn-BD" className="hello"> World </Clock>);
+
+function Welcome(props){
+    return(
+      <div>
+        <h1 className={props.classN}>Hello {props.name} {props.children}</h1>
+      </div>
+    );
+  }
+  
+  function App(){
+    return (
+      <div>
+        <Welcome name="raiyan" classN="rn"> is my name </Welcome>
+        <Welcome name="daiyan" classN="dn"> is my brother's name </Welcome>
+        <Welcome name="Kabila" classN="kn"> is my sister's name </Welcome>
+        <Welcome name="Jasiya" classN="jn"> is my sister's name </Welcome>
+      </div>
+    );
+  }
+  
+  const root=ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
