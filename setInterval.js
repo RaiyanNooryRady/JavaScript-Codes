063f609f47e4266be4e1b486a464ab560cf8c5d8
DaiyanNDahy <daiyanndahy@gmail.com>
2024-04-27 08:54:38 +0600

    set interval

--- setInterval.js
+++ setInterval.js
@@ -1,9 +1,15 @@
-let x;
 function trick() {
     let x = document.getElementById('root');
     let y = new Date().toLocaleTimeString();
     console.log(y);
-    root.innerHTML = `Hello The time is ${y}`;
+    x.innerHTML = `Hello The time is ${y}`;
 }
 trick();
 setInterval(trick, 1000);
+
+function loop(){
+    let x=document.getElementById('demo');
+    let y=`Hello World ${new Date().getSeconds()}`
+    x.innerHTML=`demo value is ${y}`;
+}
+setInterval(loop,1000);
