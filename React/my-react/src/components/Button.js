c42455138f79565c6ea8e25b53fcbab2520bd7e9
DaiyanNDahy <daiyanndahy@gmail.com>
2024-04-28 20:07:40 +0600

    conditional rendering

--- React/my-react/src/components/Button.js
+++ React/my-react/src/components/Button.js
@@ -1,19 +1,23 @@
 import React from "react";
 class Button extends React.Component {
     shouldComponentUpdate(nextProps) {
-        const { change: currentChange,locale:currentLocale } = this.props;
-        const { change: nextChange,locale:nextLocale } = nextProps;
-        if (currentChange === nextChange && currentLocale===nextLocale)
+        const { change: currentChange, locale: currentLocale } = this.props;
+        const { change: nextChange, locale: nextLocale } = nextProps;
+        if (currentChange === nextChange && currentLocale === nextLocale)
             return false;
         else return true;
     }
     render() {
-       // console.log('button component updated');
-        const { change,locale } = this.props;
+        // console.log('button component updated');
+        const { change, locale,show } = this.props;
+        //if(!enable) return null;
         return (
-
-            <button type="button" onClick={()=>change(locale)}>Click Here</button>
-
+            <>
+                <button type="button" onClick={() => change(locale)}>
+                    {locale === 'bn-BD' ? 'Change Clock' : 'ঘড়ি পরিবর্তন করুন'}
+                </button>
+                {show && <p>Hello</p> }
+            </>
         )
     }
 
--- React/my-react/src/components/Clock.js
+++ React/my-react/src/components/Clock.js
@@ -28,13 +28,13 @@ class Clock extends React.Component {
     render() {
         //console.log('clock component udpated');
         const { date,locale } = this.state;
-        let button;
-        if(locale==='bn-BD'){
-            button= <Button change={this.handleClick} locale="en-US"> Click Here </Button>;
-        }
-        else{
-            button= <Button change={this.handleClick} locale="bn-BD"> Click Here </Button>
-        }
+        // let button;
+        // if(locale==='bn-BD'){
+        //     button= <Button change={this.handleClick} locale="en-US"> Click Here </Button>;
+        // }
+        // else{
+        //     button= <Button change={this.handleClick} locale="bn-BD"> Click Here </Button>
+        // }
         return (
             //state holo component er vitorer data change kora. state is a javascript object. React.component er ekta property
             <div>
@@ -42,13 +42,13 @@ class Clock extends React.Component {
 
                     {/* <span className="text">Hello {this.props.children}{ this.state.date.toLocaleTimeString(this.props.locale)}</span> <br/> */}
                     <span className="text">{date.toLocaleTimeString(locale)}</span>
-                    <span className={this.props.className}>second is {date.getSeconds()}</span>
+                    {/* <span className={this.props.className}>second is {date.getSeconds()}</span> */}
 
                 </h1>
                 {/* <button type="button" onClick={this.handleClick.bind(this,"en-US")}>Click Here</button> */}
                 {/* <Button change={this.handleClick} locale="en-US"> Click Here </Button> */}
-                {button} <br/><br/>
-                {locale==='bn-BD'?<Button change={this.handleClick} locale="en-US"> Click Here </Button>:<Button change={this.handleClick} locale="bn-BD"> Click Here </Button>}
+                {/* {button} <br/><br/> */}
+                {locale==='bn-BD'?<Button change={this.handleClick} locale="en-US" show={false} />:<Button change={this.handleClick} locale="bn-BD" show />}
             </div>
         );
     }
