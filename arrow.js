ddc07199bef6f8b52b1a0a628b4258a1115c5fbe
DaiyanNDahy <daiyanndahy@gmail.com>
2024-06-18 15:17:21 +0600

    arrow function

---
+++ arrow.js
@@ -0,0 +1,12 @@
+let fun= (x)=>{
+return x;
+};
+let number=(...x)=>{
+    return x;
+}
+let sum=(x,y)=>x+y;
+console.log(fun('Hello'));
+console.log(number(1,2,4,5,6,3));
+console.log(sum(4,5));
+let mul=(a,b)=>console.log(a*b);
+mul(5,4);
