ffcf66722367378a781b907409f2fa34d3bb7f70
DaiyanNDahy <daiyanndahy@gmail.com>
2024-06-18 09:41:25 +0600

    let vs var vs const

--- variable.js
+++ variable.js
@@ -1,12 +1,16 @@
-let x=10;
-//let x=20; not possible as cannot be redeclared
+let x=10,glob=90;
+//let x=20; not possible as let cannot be redeclared
 var y=10;
 var y=20; //possible as var can be redeclared
-console.log(x,y);
+const z=50;
+//const z= 60; not possible as const cannot be redeclared
+//z=30; not possible as const cannot be reassigned
+console.log(x,y,z);
 
 {
     let x=20; 
-    let y=30;
-    console.log(x,y);////possible as this x,y are in local scope
+    var y=30;
+    const z=60;
+    console.log(x,y,z,glob);//these x,y,z are in local scope and glob is global variable
 }
 console.log(x,y); //this are global x,y
