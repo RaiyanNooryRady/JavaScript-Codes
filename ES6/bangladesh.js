aa3df8be452f19382bc1bf2d7d141a8519fe0b01
DaiyanNDahy <daiyanndahy@gmail.com>
2024-06-19 07:30:24 +0600

    default export and import

--- ES6/bangladesh.js
+++ ES6/bangladesh.js
@@ -1,3 +1,8 @@
-import { laptop,mobilePhone } from "./china.js";
+import { laptop, mobilePhone, China } from "./china.js";
+import Uigs from "./china.js"; //we can change name as default exported
 console.log(laptop);
 mobilePhone();
+let obj=new China();
+obj.Toys();
+let objUighur=new Uigs();
+objUighur.genocide();
--- ES6/china.js
+++ ES6/china.js
@@ -2,4 +2,15 @@ const laptop= 'Asus laptop, HP Laptop';
 function mobilePhone(){
     console.log('Mobile phone from china');
 }
-export {laptop,mobilePhone};
+class China{
+    Toys(){
+        console.log('toys imported from china');
+    }
+}
+class Uighur{
+    genocide(){
+        alert('Uighurs are tortured by china government and Han Chinese');
+    }
+}
+export {laptop, mobilePhone, China};
+export default Uighur;
